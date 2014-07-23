45441c2042c88f5ede38cf83e58ac5a8599bb978
Brandon Ward <ward486@regis.edu>
2014-07-22 21:23:31 -0600

    Added a TODO about where to edit the action bar action button in top right

--- app/src/main/java/brillianceanimationstudio/brandonward/baccalculator/NavigationDrawerFragment.java
+++ app/src/main/java/brillianceanimationstudio/brandonward/baccalculator/NavigationDrawerFragment.java
@@ -248,8 +248,8 @@ public class NavigationDrawerFragment extends Fragment{
         }
 
         if (item.getItemId() == R.id.action_example) {
-            Toast.makeText(getActivity(), "Calculate Now", Toast.LENGTH_SHORT).show();
-            // TODO: Make this "Calculate Now" and call validates and update BAC stuff, etc...
+            Toast.makeText(getActivity(), "Example Action.", Toast.LENGTH_SHORT).show();
+            // TODO: Make this "Calculate Now" and call validates and update BAC stuff, etc... and find where this originates.
             return true;
         }
 
